59c5f348e781fd2a4f7b5e598d5e3f7d335ea90f
liren-cell <1441347985@qq.com>
2021-10-01 23:52:06 +0800

    Initial commit

---
+++ blog-app/src/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="home">
+    <el-row style="height: 40px;background-color: #000000;color: #898989;text-align: center;line-height: 40px;font-size: 12px">
+    <el-col :span="12">欢迎进入优贝!</el-col>
+    <el-col :span="12">全国服务热线：020-39012145</el-col>
+    </el-row>
+    <el-container>
+
+    	<base-header :activeIndex="activeIndex"></base-header>
+
+		  <router-view class="me-container"/>
+
+			<base-footer v-show="footerShow"></base-footer>
+
+		</el-container>
+
+  </div>
+
+</template>
+
+<script>
+import BaseFooter from '@/components/BaseFooter'
+import BaseHeader from '@/views/BaseHeader'
+
+export default {
+  name: 'Home',
+  data (){
+  	return {
+  			activeIndex: '/',
+  			footerShow:true
+  	}
+  },
+  components:{
+  	'base-header':BaseHeader,
+  	'base-footer':BaseFooter
+  },
+  beforeRouteEnter (to, from, next){
+  	 next(vm => {
+    	vm.activeIndex = to.path
+  	})
+  },
+  beforeRouteUpdate (to, from, next) {
+	  if(to.path == '/'){
+	  	this.footerShow = true
+	  }else{
+	  	this.footerShow = false
+	  }
+	  this.activeIndex = to.path
+	  next()
+	}
+}
+</script>
+
+<style>
+
+.me-container{
+  margin: 100px auto 140px;
+}
+</style>
